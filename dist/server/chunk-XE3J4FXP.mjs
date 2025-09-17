5955ea59716ab8216884d749f47e8fcd19083c9f
soner.acar <soner.acar@vbt.com.tr>
2025-09-17 16:06:30 +0300

    🎯 : Genel

--- dist/server/chunk-XE3J4FXP.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-var e=class{static toFriendlyUrl(r){return r.toLowerCase().replace(/ç/g,"c").replace(/ğ/g,"g").replace(/ı/g,"i").replace(/ö/g,"o").replace(/ş/g,"s").replace(/ü/g,"u").replace(/[^a-z0-9]+/g,"-").replace(/^-+|-+$/g,"")}};export{e as a};
